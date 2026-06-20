4164a690f56e17918d3680bce9043a1e131a73f6
Bikalpa KC <bikalpa061@gmail.com>
2026-06-20 19:44:16 +0545

    Fixed a typo that crashed ECS deployment in CI/CD pipeline

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import myUser
+from .models import MyUser
 # Register your models here.
 
-@admin.register(myUser)
-class myUserAdmin(admin.ModelAdmin):
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
     list_display = ('email', 'name', 'is_active', 'is_staff', 'created_at')
     search_fields = ('email', 'name')
     list_filter = ('is_active', 'is_staff')
